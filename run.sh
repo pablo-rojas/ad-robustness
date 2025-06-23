1b49e12b199d12b1fed4fd073805027c8a2fc457
pablo-rojas <pablo.rojassed@gmail.com>
2025-06-23 13:43:38 +0200

    Moved all train and test scrips from base directory to a dedicated 'scripts' folder for better organization

--- run.sh
+++
@@ -1,3 +0,0 @@
-# python train_multiple_seeds.py --config cfg/cifar_train_us.json --type mahalanobis
-python train.py --config cfg/svhn_train_us.json
-python train.py --config cfg/svhn_train_us.json
